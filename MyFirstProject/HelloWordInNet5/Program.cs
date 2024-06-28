ce8e993fd7733a786abe0ad82b4fc32a081f316e
Eric O. <erico49867@gmail.com>
2024-06-27 21:53:23 -0500

    Unimportant updates

---
+++ MyFirstProject/HelloWordInNet5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWordInNet5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
