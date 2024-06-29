36089889c7cfd0d3deceefaf9450b663292f07e8
Eric O. <erico49867@gmail.com>
2024-06-29 11:12:05 -0500

    List and Dictionaries

---
+++ DictionaryApp/Dictionary/Program.cs
@@ -0,0 +1,70 @@
+﻿
+Dictionary<int, string> lookup = new Dictionary<int, string>();
+
+Dictionary<string, string> animalHuman = new Dictionary<string, string>();
+
+//lookup.Add(1, "Tom");
+//lookup.Add(2, "Jerry");
+//lookup.Add(3, "cat");
+//lookup.Add(4, "mouse");
+
+animalHuman["animal"] = "Not a human";
+animalHuman["fish"] = "Not a human that swims";
+animalHuman["human"] = "Us";
+
+Console.WriteLine($"The definiont of fish is {animalHuman["fish"]}");
+
+
+//Console.WriteLine($"The employee with ID number 28 is {employees[28]}.");
+
+Dictionary<string, int> dayOfWeek = new Dictionary<string, int>();
+
+dayOfWeek["Wednesday"] = 4;
+dayOfWeek["Thursday"] = 5;
+dayOfWeek["Friday"] = 6;
+
+Console.WriteLine($"Wednesday is day number {dayOfWeek["Wednesday"]}");
+
+
+// Create a dictionary list of employee IDs and the name that goes with the ID
+// Ask the user for their id and return their name, make sure to check for invalid answers
+
+
+//Dictionary<int, string> employees = new Dictionary<int, string>();
+//employees[95] = "Jim Jones";
+//employees[50] = "Sue Storm";
+//employees[28] = "Tom Cat";
+//employees[29] = "Rick Ross";
+//employees[30] = "Jody Highroller";
+
+Dictionary<int, string> employees = new Dictionary<int, string>
+        {
+            { 95, "Jim Jones" },
+            { 50, "Sue Storm" },
+            { 28, "Tom Cat" },
+            { 29, "Rick Ross" },
+            { 30, "Jody Highroller" }
+        };
+
+int userID;
+int count = 0;
+
+Console.WriteLine("What is your employee ID?");
+while(count != 1)
+{
+    int.TryParse(Console.ReadLine(), out userID);
+
+    // Check if the userID exists in the dictionary
+    if(employees.ContainsKey(userID))
+    {
+        string userName = employees[userID];
+        Console.WriteLine($"Welcome, {userName}!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid ID. Please enter a valid employee ID.");
+    }
+}
+
+ 
---
+++ ListsApp/Lists/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+List<string> firstNames = new List<string>();
+
+firstNames.Add("Tim");
+firstNames.Add("Adam");
+firstNames.Add("Sue");
+
+Console.WriteLine(firstNames[firstNames.Count - 1]);    
+
+List<int> ages = new List<int>();
+ages.Add(1);    
+ages.Add(2);
+ages.Add(3);
+ages.Add(4);
+
+// List<T> - generic
+
+string data = "Corey,Smith,Jones";
+
+List<string> lastNames = data.Split(',').ToList();
+lastNames.Add("Franklin");
+
+for (int i = 0; i < lastNames.Count; i++)
+{
+    Console.WriteLine(lastNames[i]);
+}
+Console.WriteLine();
+
+lastNames.Add(lastNames[0] = "Johnson"); // replaces Sue with Johnson
+
+for (int i = 0; i < lastNames.Count; i++)
+{
+    Console.WriteLine(lastNames[i]);
+}
+Console.WriteLine();
+
+
+// Add students to a class roster List until there are no more students.
+// Then print out the count of the students to the console.
+
+
+string students = "Jim,John,Sue,Tom,Mark";
+List<string> roster = new List<string>();
+
+// Split the students string and add them to the roster list
+roster.AddRange(students.Split(','));
+
+// Print out the count of students in the roster list
+Console.WriteLine($"Number of students: {roster.Count}");
