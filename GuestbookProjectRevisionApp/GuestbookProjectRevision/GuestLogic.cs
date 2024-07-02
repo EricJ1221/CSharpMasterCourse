2696a3b850e060a4ea0f1f4b30563b9ce8ab23c7
Eric O. <erico49867@gmail.com>
2024-07-02 12:48:30 -0500

    Finished revising my Guest Book Project

---
+++ GuestbookProjectRevisionApp/GuestbookProjectRevision/GuestLogic.cs
@@ -0,0 +1,91 @@
+﻿
+
+
+namespace GuestbookProjectRevision
+{
+    public static class GuestLogic
+    {
+        public static void WelcomeMessage()
+        {
+            // - Welcome the user 
+            // -- intro the app name 
+
+            Console.WriteLine("Welcome to the Guest Book App.");
+            Console.WriteLine("******************************");
+            Console.WriteLine();
+        }
+
+        public static string GetPartyName()
+        {
+            // - ask user for their name
+            // -- store the name
+
+            Console.WriteLine("What is your party/group name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        public static int GetPartySize()
+        {
+            // - ask user for number in their party
+            // -- store the number
+            // -- convert string to a number (int)
+            bool isValidNumber;
+            int output;
+            do
+            {
+                Console.WriteLine("How many people are in your party: ");
+                string partySizeText = Console.ReadLine();
+                isValidNumber = int.TryParse(partySizeText, out output); 
+                if (!isValidNumber)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+            } while (isValidNumber = false);
+
+            return output;
+        }
+        
+        public static (List<string> guests, int total) GetAllGuests()
+        {
+            int totalGuests = 0;
+            List<string> guests = new List<string>();   
+
+            do
+            {
+                guests.Add(GetPartyName());
+
+                totalGuests += GetPartySize();
+
+            } while (AskToContinue());
+
+            return (guests, totalGuests);
+        }
+
+        public static bool AskToContinue()
+        {
+            Console.WriteLine("Are there more guests coming (yes/no): ");
+            string continueLooping = Console.ReadLine();
+            Console.WriteLine(); 
+
+            bool output = (continueLooping.ToLower() == "yes");
+            return output;
+        }
+
+        public static void DisplayGuests(List<string> guests)
+        {
+            foreach (string guest in guests)
+            {
+                Console.WriteLine($"The {guest} Family.");
+            }
+        }
+
+        public static void DisplayGuestCount(int totalGuests)
+        {
+            Console.WriteLine("Thank you for everyone who attended.");
+            Console.WriteLine($"The total guest count for this evening was {totalGuests}.");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ GuestbookProjectRevisionApp/GuestbookProjectRevision/Program.cs
@@ -0,0 +1,27 @@
+﻿// This is a revision of the GuestBookProject I created previously.
+// Some of the requirements were implied and can be implemented more efficiently.
+
+
+using GuestbookProjectRevision;
+
+
+
+GuestLogic.WelcomeMessage();
+
+var (guests, totalGuests) = GuestLogic.GetAllGuests();
+
+GuestLogic.DisplayGuests(guests);
+
+GuestLogic.DisplayGuestCount(totalGuests);  
+
+
+
+
+// - add the number to total
+
+// - Check to see if more guests are coming
+// -- loop back to the top until no more guests
+
+// - Loop over the names list and print them
+
+// - Print the total guest count
