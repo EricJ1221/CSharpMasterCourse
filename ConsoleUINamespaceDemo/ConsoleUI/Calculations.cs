95444a16ae70167bd71c750dd8881a73210a3a75
Eric O. <erico49867@gmail.com>
2024-07-12 14:18:00 -0500

    Namespaces Walkthrough

---
+++ ConsoleUINamespaceDemo/ConsoleUI/Calculations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationInfo.Calculators.PersonalCalculators.EricsCalculators;
+
+public class Calculations
+{
+    public static double Add(double x, double y) 
+    {
+        return x + y;
+    }
+}
---
+++ ConsoleUINamespaceDemo/ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleUI.Models;
+using FoundationInfo;
+using FoundationInfo.Calculators.PersonalCalculators.EricsCalculators; // A little ridiculous, but it works.
+using System;
+using System.Collections.Generic;
+
+// < > - indicates a Generic
+// use namespaces to organize your application logically
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            // System.Collections.Generic.List<PersonModel> people = new System.Collections.Generic.List<PersonModel>(); // Without utilising the using statement.
+            List<PersonModel> people = new List<PersonModel>();
+
+            // System.Console.WriteLine();  // Without utilising the using statement.
+            Console.WriteLine();
+
+            PersonModel person = new PersonModel();
+
+            // FoundationInfo.Calculations.Add(4, 3); // Similar to import statements, but not the same. Allows us to use another namespace aside from the current namespace.
+            Calculations.Add(4, 3);
+
+            Console.ReadLine();
+        }
+
+    }
+}
