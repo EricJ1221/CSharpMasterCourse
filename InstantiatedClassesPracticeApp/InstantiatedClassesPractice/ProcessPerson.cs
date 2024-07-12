d48026ad2595ee35010579e6b7a9247dc51d7c77
Eric O. <erico49867@gmail.com>
2024-07-12 12:12:48 -0500

    Instantiated Classes Practice

---
+++ InstantiatedClassesPracticeApp/InstantiatedClassesPractice/ProcessPerson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantiatedClassesPractice
+{
+    internal class ProcessPerson
+    {
+        public static void GreetPerson(PersonModel person)
+        {
+            Console.WriteLine($"Welcome to my application {person.FirstName} {person.LastName}.");
+            person.HasBeenWelcomed = true;
+
+        }
+
+        public static Address GetAddress(string firstName, string lastName) 
+        {
+            Console.WriteLine($"{firstName} {lastName}, what is your street?");
+            string street = Console.ReadLine();
+            
+            Console.WriteLine("What is your city?");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("What is your state?");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("What is your zip code?");
+            string zipCode = Console.ReadLine();
+
+            return new Address
+            {
+                Street = street,
+                City = city,
+                State = state,
+                ZipCode = zipCode
+            };
+        }
+    }
+}
---
+++ InstantiatedClassesPracticeApp/InstantiatedClassesPractice/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+
+
+// Create a Console application that has a person class and an Address class
+
+using InstantiatedClassesPractice;
+
+namespace ConsoleUI
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            string firstName = "";
+
+            do
+            {
+                Console.WriteLine("What is your first name? (type exit to stop)");
+                firstName = Console.ReadLine();
+
+                if (firstName.ToLower() == "exit")
+                {
+                    break;  // Exit the loop if the user types "exit"
+                }
+
+                Console.WriteLine("What is your last name?");
+                string lastName = Console.ReadLine();
+
+                Address address = ProcessPerson.GetAddress(firstName, lastName);
+
+                PersonModel person = new PersonModel
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    PersonAddress = address
+                };
+
+                people.Add(person);
+
+            } while (true);  // The loop will break internally based on the input
+
+            foreach (PersonModel p in people) 
+            {
+                Console.WriteLine($"{p.FirstName} {p.LastName} lives at {p.PersonAddress.Street} {p.PersonAddress.City}, {p.PersonAddress.State}, {p.PersonAddress.ZipCode}.");
+                ProcessPerson.GreetPerson(p);
+            } 
+            
+            Console.ReadLine();
+
+        }
+    }
+}
