38303475cddb8d613f5c833a9b4684c1f0672643
Eric O. <erico49867@gmail.com>
2024-07-12 17:35:46 -0500

    Refactored previous project
    Refactored previous Guestbook Application. Recreated from memory to test my memory and skills.

---
+++ ConsoleUIBetterGuestBookApp/ConsoleUI/Program.cs
@@ -0,0 +1,68 @@
+﻿
+// Capture the information about each guest (assumption, at least one guest, unknown maximum)
+// Info to Capture: First name, Last name, Message to the host
+// Once done, loop through each guest and print their info
+
+// Design your app with just UI content in the UI. Put everything else ina class library.
+
+using GuestBookLibrary.Models;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        private static List<GuestModel> guests = new List<GuestModel>(); // variables can live outside of the method and is now in scope after our refactoring
+           // probably not the best practice if memory is a concern                                                         
+        static void Main(string[] args) 
+        { 
+           
+            GetGuestInformation();
+
+            PrintGuestInformation();
+
+
+            Console.ReadLine();
+
+        }
+        private static void PrintGuestInformation()
+        {
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+        }
+        private static void GetGuestInformation()
+        {
+            string moreGuestsComing = "";
+            do
+            {
+                GuestModel guest = new GuestModel();
+
+                //Console.WriteLine("What is your first name?"); // In reality you would want to validate all data being entered for storage
+                guest.FirstName = GetInfoFromConsole("What is your first name?");
+
+                guest.LastName = GetInfoFromConsole("What is your last name?");
+
+                guest.MessageToHost = GetInfoFromConsole("What is your message to the host?");
+
+                moreGuestsComing = GetInfoFromConsole("Are more guests coming? (yes/no)");
+
+                guests.Add(guest);
+
+                Console.Clear();
+
+            } while (moreGuestsComing.ToLower() == "yes");
+        }
+
+        private static string GetInfoFromConsole(string message)
+        {
+            string output = "";
+
+            Console.Write(message);
+            output = Console.ReadLine();
+
+            return output;
+                
+        }
+    }
+}
---
+++ GuestBookImprovementsPracticeApp/GuestBookImprovementsPractice/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using GuestbookLibrary;
+using GuestbookLibrary.Models;
+
+// recreate the guest book project without looking back
+// take the concept and Create the application
+
+// Capture the information about each guest (assumption, at least one guest, unknown maximum)
+// Info to Capture: First name, Last name, Message to the host
+// Once done, loop through each guest and print their info
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        private static List<GuestModel> guests = new List<GuestModel>(); // instantiate this in the class so that it can be used by class methods
+        static void Main(string[] args) 
+        {
+            GetGuestInformation();
+
+            PrintGuestInformation();
+
+            Console.ReadLine();
+
+        }
+        private static void GetGuestInformation()
+        {
+            string moreGuestsComing = "";
+            do
+            {
+                GuestModel guest = new GuestModel();
+                guest.FirstName = GetInfoFromConsole("What is your first name?");
+
+
+                guest.LastName = GetInfoFromConsole("What is your last name?");
+
+                guest.MessageToHost = GetInfoFromConsole("What is your message to the host?");
+
+                moreGuestsComing = GetInfoFromConsole("Are there more guests coming? (yes/no)");
+                
+                guests.Add(guest);
+
+            } while (moreGuestsComing.ToLower() == "yes");
+
+        }
+
+        private static string GetInfoFromConsole(string message) // helper method to grab guest info from the Console
+        {
+            string output = "";
+            Console.Write(message);
+            output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void PrintGuestInformation()
+        {
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+        }
+    }
+}
