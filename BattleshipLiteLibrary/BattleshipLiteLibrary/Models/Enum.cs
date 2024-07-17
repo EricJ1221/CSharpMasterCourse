9f4d511022f12669cf17d0b980fa827e75b4a5a9
Eric O. <erico49867@gmail.com>
2024-07-17 10:25:25 -0500

    Building out BattleshipLite App
    Fixed mistyped name of library

---
+++ BattleshipLiteLibrary/BattleshipLiteLibrary/Models/Enum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary.Models
+{
+
+    // enum explanation -- 0 = empty, 1 = ship, 2 = miss, 3 = hit, 4 = sunk
+    public enum GridSpotStatus // Could set empty = 5 to override the usual enum of 1,2,3 etc.
+    {
+        Empty,
+        Ship,
+        Miss,
+        Hit,
+        Sunk
+    }
+
+}
