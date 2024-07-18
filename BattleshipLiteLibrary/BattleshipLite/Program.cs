9bc90165d2f3effe231073cd675c1ec3ce08e814
Eric O. <erico49867@gmail.com>
2024-07-18 16:48:53 -0500

    Building out the game
    I added in quite a few methods in the program.cs file as well as 5 methods to the GameLogic file that need to be fleshed out. I have more pressing matters of training my Multimodal Transformer Ensemble Model at this time.

--- BattleshipLiteLibrary/BattleshipLite/Program.cs
+++ BattleshipLiteLibrary/BattleshipLite/Program.cs
@@ -9,10 +9,124 @@ namespace BattleshipLite
         {
             WelcomeMessage();
 
-            PlayerInfoModel player1 = CreatePlayer("Player 1");
-            PlayerInfoModel player2 = CreatePlayer("Player 2");
+            PlayerInfoModel activePlayer = CreatePlayer("Player 1");
+            PlayerInfoModel opponent = CreatePlayer("Player 2");
+            PlayerInfoModel winner = null;
+
+            do
+            {
+                // Display the grid from activePlayer on where they fired
+                DisplayShotGrid(activePlayer);
+
+                // Ask activePlayer for a shot
+                // Determine if it is a valid shot
+                // Determine shot results
+                RecordPlayerShot(activePlayer, opponent);
+
+                // Determine if the game is over
+                // The game is over if your opponent has no more ships
+                // I need to check if the ship locations for the opponent are sunk
+                // I need to make a GameLogic method, because this has nothing to do with the user interface
+                bool doesGameContinue = GameLogic.PlayerStillActive(opponent);
+
+                // If over, set activeplayer as the winner
+                // else, swap positions (activePlayer to opponent)
+                if (doesGameContinue == true)
+                {
+                    // Swapping player position with temp variable
+                    // PlayerInfoModel tempHolder = opponent; 
+                    // opponent = activePlayer;
+                    // activePlayer = tempHolder;
+
+                    // Swapping player position with a tuple
+                    (activePlayer, opponent) = (opponent, activePlayer);
+
+                }
+                else
+                {
+                    winner = activePlayer;
+                }
+
+            } while (winner == null);
+
+            IdentifyWinner(winner);
 
             Console.ReadLine();
+        }
+
+        private static void IdentifyWinner(PlayerInfoModel winner)
+        {
+            Console.WriteLine($"Congratulations to {winner.UsersName} for winning!");
+            Console.WriteLine($"{winner.UsersName} took {GameLogic.GetShotCount(winner)} shots.");
+        }
+
+        private static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
+        {
+            // Asks for a shot (We ask for "B2")
+            // Determine what row and column that is = split it apart
+            // Determine if that is a valid shot
+            // Go back to the beginning if not a valid shot
+            bool isValidShot = false;
+            string row = "";
+            int column = 0;
+
+            do
+            {
+               string shot = AskForShot();
+               (row, column) = GameLogic.SplitShotIntoRowAndColumn(shot);
+               isValidShot = GameLogic.ValidateShot(activePlayer,row, column);
+               
+                if (isValidShot == false)
+               {
+                    Console.WriteLine("Invalid shot location. Please try again.");
+               }
+            } while (isValidShot == false);
+
+            // Determine Shot Results
+            bool isAhit = GameLogic.IdentifyShotResult(opponent, row, column);
+
+            // Record results
+            GameLogic.MarkShotResult(activePlayer, row, column, isAhit);
+
+            // If we want to refresh the shot grid everytime, just add in the DisplayShotGrid Method
+        }
+
+        private static string AskForShot()
+        {
+            Console.Write("Please enter your shot selection: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void DisplayShotGrid(PlayerInfoModel activePlayer) // I added this here and not in the library because it is going to display to the console
+        {
+            string currentRow = activePlayer.ShotGrid[0].SpotLetter;
+            
+            foreach (var gridSpot in activePlayer.ShotGrid)
+            {
+                if (gridSpot.SpotLetter != currentRow) 
+                {
+                    Console.WriteLine();
+                    currentRow = gridSpot.SpotLetter;
+                }
+               if(gridSpot.Status == GridSpotStatus.Empty)
+                {
+                    Console.Write($" {gridSpot.SpotLetter}{gridSpot.SpotNumber}");
+                }
+               else if (gridSpot.Status == GridSpotStatus.Hit) 
+                {
+                    Console.Write(" X ");
+                }
+               else if (gridSpot.Status == GridSpotStatus.Miss) 
+                {
+                    Console.Write(" O ");
+                }
+               else
+                {
+                    Console.Write(" ? ");
+                }
+            }
         }
 
         private static void WelcomeMessage()
