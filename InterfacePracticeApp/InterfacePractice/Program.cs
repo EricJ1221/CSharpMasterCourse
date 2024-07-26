6ba3db50c7ee12fb608209335c6931e02ad7f54c
Eric O. <erico49867@gmail.com>
2024-07-26 14:55:13 -0500

    Interface Practice

---
+++ InterfacePracticeApp/InterfacePractice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InterfacePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IRun> runners = new List<IRun>()
+            {
+                new Person {Name = "John"},
+                new Person {Name = "Eric"},
+                new Animal {Species = "Cheetah"},
+                new Animal {Species = "Elephant"}
+            };
+
+            foreach (var runner in runners) 
+            { 
+                runner.Run();
+            }
+        }
+    }
+
+    public interface IRun
+    {
+        void Run();
+    }
+
+    public class Person : IRun
+    {
+        public string Name { get; set; }
+        public void Run()
+        {
+            Console.WriteLine($"{Name} is running.");
+        }
+    }
+    public class Animal : IRun
+    {
+        public string Species { get; set; }
+        public void Run()
+        {
+            Console.WriteLine($"The {Species} is running.");
+        }
+    }
+}
